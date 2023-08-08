d053f4e1fd0295ac16987c502760e753adc22592
Estefane Freitas <estefane.freitas.dev@gmail.com>
2023-08-08 17:08:57 -0300

    atualização background baseado no media queries

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const image = {
-  foto1: ".//assents/avatar.png",
-  foto2: ".//assents/foto2.png",
-  alt:"foto de estefane"
+  foto1: ".//assets/avatar.png",
+  foto2: ".//assets/foto2.png",
+  alt: "foto de estefane"
 }
 
-function toggleMode () {
+function toggleMode() {
   const html = document.body
   html.classList.toggle("light")
 
   const img = document.querySelector('#profile img')
 
-  if(html.classList.contains("light")){
-    img.setAttribute("src",image.foto2 )
-    img.setAttribute("title",image.alt)
-    }
-  else{
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", image.foto2)
+    img.setAttribute("title", image.alt)
+  }
+  else {
     img.setAttribute("src", image.foto1)
   }
 }
-function loadImg(){
-  const img = document.querySelector("#profile img") 
-  
+function loadImg() {
+  const img = document.querySelector("#profile img")
+
   img.setAttribute("src", image.foto1)
   img.setAttribute("title", image.alt)
 }
